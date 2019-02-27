f653450c145d6a7ba5894b95bcd525d6091aa857
Yazen Nasr <ynasr74@gmail.com>
2019-02-27 19:16:31 -0400

    First commit of the working foundation.

---
+++ Fall Game/Assets/Scripts/Bird.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce = 100f;
+    public float maxSpeed = 5f;
+    bool facingRight = true;
+    private bool isDead = false;
+    private Rigidbody2D m_Rigidbody2D;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        m_Rigidbody2D.velocity = new Vector2(move * maxSpeed, m_Rigidbody2D.velocity.y);
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                m_Rigidbody2D.velocity = Vector2.zero;
+                m_Rigidbody2D.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        m_Rigidbody2D.velocity = Vector2.zero;
+        m_Rigidbody2D.gravityScale = 0;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+}
---
+++ Fall Game/Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D wallCollider;
+    private float wallVerticalLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wallCollider = GetComponent<BoxCollider2D>();
+        wallVerticalLength = wallCollider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= wallVerticalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 wallOffset = new Vector2(0, -wallVerticalLength);
+        transform.position = (Vector2)transform.position + wallOffset;
+    }
+
+    /* public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        Debug.Log(startPosition);
+    }
+
+    private void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    } */
+}
